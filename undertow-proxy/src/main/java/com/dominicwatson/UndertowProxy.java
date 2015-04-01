8fedf21c9baa02115fddacb82a4cb1c14e6ca04a
Dominic Watson <watson.dominic@gmail.com>
2015-04-01 19:54:37 +0100

    A little refactoring for legibility

--- undertow-proxy/src/main/java/com/dominicwatson/UndertowProxy.java
+++ undertow-proxy/src/main/java/com/dominicwatson/UndertowProxy.java
@@ -12,20 +12,25 @@ public class UndertowProxy {
         server.start();
     }
 
-    private static HttpHandler getHttpHandler() {
-    	return new HttpHandler() {
+    private static Undertow buildServer() {
+        Builder builder = getServerBuilder();
+
+        builder.addHttpListener( 3000, "localhost" );
+        builder.setHandler( getHelloWorldHttpHandler() );
+
+        return builder.build();
+    }
+
+    private static Builder getServerBuilder() {
+        return Undertow.builder();
+    }
+
+    private static HttpHandler getHelloWorldHttpHandler() {
+        return new HttpHandler() {
             public void handleRequest( final HttpServerExchange exchange ) throws Exception {
                 exchange.getResponseHeaders().put( Headers.CONTENT_TYPE, "text/plain" );
                 exchange.getResponseSender().send( "Hello World" );
             }
         };
-    }
-
-    private static Undertow buildServer() {
-        Builder serverBuilder = Undertow.builder()
-            .addHttpListener( 3000, "localhost" )
-            .setHandler( getHttpHandler() );
-
-        return serverBuilder.build();
     }
 }
