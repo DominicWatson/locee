dc0156b78a477ca5be5eaff84ab3c87a47763b3b
Dominic Watson <watson.dominic@gmail.com>
2015-04-11 15:40:09 +0100

    Refinements to the build such that everything can built through grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,28 @@ module.exports = function (grunt) {
 	'use strict';
 
 	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.loadNpmTasks('grunt-exec');
 
-	grunt.registerTask( 'default', [ 'nodewebkit' ] );
+	grunt.registerTask( 'default', [ 'exec:install_jar', 'nodewebkit' ] );
+	grunt.registerTask( 'jars', [ 'exec:install_jar' ] );
+	grunt.registerTask( 'linux', [ 'nodewebkit:linux' ] );
+	grunt.registerTask( 'win', [ 'nodewebkit:win' ] );
+	grunt.registerTask( 'osx', [ 'nodewebkit:osx' ] );
 
 	grunt.initConfig( {
 		nodewebkit: {
-			options: {
-				platforms: [ 'win', 'osx', 'linux' ],
-				buildDir: './builds',
-				version: '0.11.6'
-			},
-			src: ['./app/**/*'] // Your node-webkit app
+			linux: { src: ['./app/**/*'], options : { platforms : [ 'linux' ], buildDir: './builds', version: '0.11.6' } },
+			win  : { src: ['./app/**/*'], options : { platforms : [ 'win'   ], buildDir: './builds', version: '0.11.6' } },
+			osx  : { src: ['./app/**/*'], options : { platforms : [ 'osx'   ], buildDir: './builds', version: '0.11.6' } }
+		},
+
+		exec : {
+			install_jar : {
+				command : 'mvn package',
+				stdout  : true,
+				cwd     : './undertowServerFactory/',
+				stderr  : true
+			}
 		}
 	} );
 };
--- app/js/serverFactory.js
+++ app/js/serverFactory.js
@@ -2,7 +2,7 @@ window.serverFactory = ( function( java ){
     "use strict";
 
     var ServerFactory = function (){
-    	java.classpath.push( "./lib/locee-undertow-server-factory-1.0-SNAPSHOT-jar-with-dependencies.jar" );
+    	java.classpath.push( "./lib/locee-undertow-server-factory.jar" );
     };
 
     ServerFactory.prototype.newServer = function( port, libDirList, webroot, webXmlPath, webInfDir ){
