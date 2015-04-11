4f328688f69fc04edef089fa6ddba874a48a1def
Dominic Watson <watson.dominic@gmail.com>
2015-04-11 11:49:29 +0100

    Undoing proof of concept code

--- app/js/application.js
+++ app/js/application.js
@@ -1,25 +1,6 @@
-window.locee = ( function( sf, $ ) {
+window.locee = ( function() {
     'use strict';
 
-    var server            = sf.newServer()
-      , $serverControlBtn = $( '#server-control' );
+    // TODO
 
-	$serverControlBtn.data( "running", false );
-
-	$serverControlBtn.on( "click", function(){
-		if ( $serverControlBtn.data( "running" ) ) {
-			server.stop();
-			$serverControlBtn.removeClass( "btn-danger" );
-			$serverControlBtn.addClass( "btn-success" );
-			$serverControlBtn.html( "Start server" );
-			$serverControlBtn.data( "running", false )
-		} else {
-			server.start();
-			$serverControlBtn.removeClass( "btn-success" );
-			$serverControlBtn.addClass( "btn-danger" );
-			$serverControlBtn.html( "Stop server" );
-			$serverControlBtn.data( "running", true )
-		}
-	} );
-
-})( window.serverFactory, jQuery );
+})();
