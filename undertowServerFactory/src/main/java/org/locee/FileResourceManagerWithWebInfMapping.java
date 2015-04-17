a2758cda6550ba44fba31af12d45c976e2659f6b
Dominic Watson <watson.dominic@gmail.com>
2015-04-17 09:17:36 +0100

    Whitespace

--- undertowServerFactory/src/main/java/org/locee/FileResourceManagerWithWebInfMapping.java
+++ undertowServerFactory/src/main/java/org/locee/FileResourceManagerWithWebInfMapping.java
@@ -7,18 +7,18 @@ import io.undertow.server.handlers.resource.FileResourceManager;
 import io.undertow.server.handlers.resource.Resource;
 
 public class FileResourceManagerWithWebInfMapping extends FileResourceManager {
-    private File WEBINF = null;
+	private File WEBINF = null;
 
-    public FileResourceManagerWithWebInfMapping( File base, long transferMinSize, File webInfDir) {
-        super( base, transferMinSize );
-        this.WEBINF = webInfDir;
-    }
+	public FileResourceManagerWithWebInfMapping( File base, long transferMinSize, File webInfDir) {
+		super( base, transferMinSize );
+		this.WEBINF = webInfDir;
+	}
 
-    public Resource getResource(String path) {
-        if ( path.startsWith( "/WEB-INF" ) ) {
-            File reqFile = new File( WEBINF, path.replace( "/WEB-INF", "" ) );
-            return new FileResource( reqFile, this, path );
-        }
-        return super.getResource(path);
-    }
+	public Resource getResource(String path) {
+		if ( path.startsWith( "/WEB-INF" ) ) {
+			File reqFile = new File( WEBINF, path.replace( "/WEB-INF", "" ) );
+			return new FileResource( reqFile, this, path );
+		}
+		return super.getResource(path);
+	}
 }
--- undertowServerFactory/src/main/java/org/locee/LoceeServletBuilder.java
+++ undertowServerFactory/src/main/java/org/locee/LoceeServletBuilder.java
@@ -18,54 +18,54 @@ import io.undertow.server.handlers.resource.FileResourceManager;
 import static io.undertow.servlet.Servlets.deployment;
 
 public class LoceeServletBuilder {
-    public static DeploymentInfo build( String libDirs, String webroot, String webXmlPath, String webInfPath ) throws IOException {
-        DeploymentInfo servletInfo = deployment();
-        URLClassLoader classLoader = buildClassLoader( libDirs );
+	public static DeploymentInfo build( String libDirs, String webroot, String webXmlPath, String webInfPath ) throws IOException {
+		DeploymentInfo servletInfo = deployment();
+		URLClassLoader classLoader = buildClassLoader( libDirs );
 
-        servletInfo.setContextPath( "" );
-        servletInfo.setClassLoader( classLoader );
-        servletInfo.setDeploymentName( webroot );
-        servletInfo.setResourceManager( new FileResourceManagerWithWebInfMapping( new File( webroot ), 100, new File( webInfPath ) ) );
-        servletInfo.addServlet( getDefaultUndertowServlet() );
+		servletInfo.setContextPath( "" );
+		servletInfo.setClassLoader( classLoader );
+		servletInfo.setDeploymentName( webroot );
+		servletInfo.setResourceManager( new FileResourceManagerWithWebInfMapping( new File( webroot ), 100, new File( webInfPath ) ) );
+		servletInfo.addServlet( getDefaultUndertowServlet() );
 
-        WebXmlToUndertowDeploymentReader.readWebXml( new File( webXmlPath ), servletInfo );
+		WebXmlToUndertowDeploymentReader.readWebXml( new File( webXmlPath ), servletInfo );
 
-        return servletInfo;
-    }
+		return servletInfo;
+	}
 
-    private static URLClassLoader buildClassLoader( String libDirs ) throws IOException {
-        List<URL> jarList  = getJarListFromLibDirectories( libDirs );
-        int       jarCount = jarList.size();
+	private static URLClassLoader buildClassLoader( String libDirs ) throws IOException {
+		List<URL> jarList  = getJarListFromLibDirectories( libDirs );
+		int       jarCount = jarList.size();
 
-        return new URLClassLoader( jarList.toArray( new URL[ jarCount ] ) );
-    }
+		return new URLClassLoader( jarList.toArray( new URL[ jarCount ] ) );
+	}
 
-    private static List<URL> getJarListFromLibDirectories( String libDirs ) throws IOException {
-        List<URL> jarList             = new ArrayList<URL>();
-        String[]  libDirArray         = libDirs.split( "," );
+	private static List<URL> getJarListFromLibDirectories( String libDirs ) throws IOException {
+		List<URL> jarList             = new ArrayList<URL>();
+		String[]  libDirArray         = libDirs.split( "," );
 
-        for ( String dirPath : libDirArray ) {
-            if ( ".".equals( dirPath ) || "..".equals( dirPath ) )
-                continue;
+		for ( String dirPath : libDirArray ) {
+			if ( ".".equals( dirPath ) || "..".equals( dirPath ) )
+				continue;
 
-            File dir = new File( dirPath );
-            for( File file : dir.listFiles() ) {
-                if ( !file.isDirectory() ) {
-                    String fileName = file.getAbsolutePath().toLowerCase();
-                    if ( fileName.endsWith( ".jar" ) || fileName.endsWith( ".zip" ) ) {
-                        jarList.add( file.toURI().toURL() );
-                    }
-                }
-            }
-        }
+			File dir = new File( dirPath );
+			for( File file : dir.listFiles() ) {
+				if ( !file.isDirectory() ) {
+					String fileName = file.getAbsolutePath().toLowerCase();
+					if ( fileName.endsWith( ".jar" ) || fileName.endsWith( ".zip" ) ) {
+						jarList.add( file.toURI().toURL() );
+					}
+				}
+			}
+		}
 
-        return jarList;
-    }
+		return jarList;
+	}
 
-    private static ServletInfo getDefaultUndertowServlet() {
-        return new ServletInfo(
-              io.undertow.servlet.handlers.ServletPathMatches.DEFAULT_SERVLET_NAME
-            , DefaultServlet.class
-        ).addInitParam( "directory-listing", "true" );
-    }
+	private static ServletInfo getDefaultUndertowServlet() {
+		return new ServletInfo(
+			  io.undertow.servlet.handlers.ServletPathMatches.DEFAULT_SERVLET_NAME
+			, DefaultServlet.class
+		).addInitParam( "directory-listing", "true" );
+	}
 }
